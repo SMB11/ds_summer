6daf8d837355be155c5c91fae54f4bccf1356df3
smb11 <sgardilyan@gmail.com>
2020-06-29 01:58:45 +0400

    mm

--- fullandMoisure.py
+++ fullandMoisure.py
@@ -179,7 +179,7 @@ while True:
     i = i+1
     now = datetime.now()
     file.write(str(now)+","+str(read_temp())+","+str(ADC.read(0)) +
-               ","+str(humidity)+","+str(temperature)+","+str(ADC.read(1))+","+str(ADC.read(2)) + "\n")
+               ","+str(result.humidity)+","+str(temperature)+","+str(ADC.read(1))+","+str(ADC.read(2)) + "\n")
     print(str(now)+","+str(read_temp())+","+str(ADC.read(0)) +
           ","+str(humidity)+","+str(temperature)+","+str(ADC.read(1))+","+str(ADC.read(2)) + "\n")
     file.flush()
