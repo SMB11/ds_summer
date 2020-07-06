3cc586a78dd071e48f7a777e2f1e40eb82e1eea1
smb11 <sgardilyan@gmail.com>
2020-07-07 00:01:43 +0400

    typo

--- dataWithAws.py
+++ dataWithAws.py
@@ -211,8 +211,6 @@ while True:
 
     file.write(str(now)+","+str(read_temp())+","+str(ADC.read(0)) +
                ","+str(humidity)+","+str(temperature)+","+str(ADC.read(1))+","+str(ADC.read(2)) + ","+"1" + "\n")
-    obj = MyDb()
-    obj.put(Date=str(now))
     print(str(now)+","+str(read_temp())+","+str(ADC.read(0)) +
           ","+str(humidity)+","+str(temperature)+","+str(ADC.read(1))+","+str(ADC.read(2)) + ","+"1" + "\n")
     file.flush()
