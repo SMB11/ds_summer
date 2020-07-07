e3e3ab3b33ca128f7152f2229111a18ca2c30adf
smb11 <sgardilyan@gmail.com>
2020-07-07 20:23:25 +0400

    tf

--- dataWithAws.py
+++ dataWithAws.py
@@ -217,7 +217,7 @@ while True:
     print(str(now)+","+str(read_temp())+","+str(ADC.read(0)) +
           ","+str(humidity)+","+str(temperature)+","+str(ADC.read(1))+","+str(ADC.read(2)) + ","+"1" + "\n")
     file.flush()
-    time.sleep(3600)
+    time.sleep(10)
 
 file.close()
 
